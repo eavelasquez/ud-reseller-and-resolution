9ca9e1e07986ca30fd49935621821fa8a3b7e737
Esteban Velasquez <eavc.30@gmail.com>
2023-02-21 16:43:15 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UD Reseller and Resolution
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App () {
+  return (
+    <div className='App'>
+      <h1>UD Reseller and Resolution</h1>
+    </div>
+  )
+}
+
+export default App
